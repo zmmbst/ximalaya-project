4647c564df06221b10d458ce3a1ed284aa913068
赵明木 <15713943729@163.com>
2024-02-22 16:03:47 +0800

    ele

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,8 @@
 import { createApp } from "vue";
+import Vant from "vant";
+import ElementPlus from 'element-plus'
 import App from "./App.vue";
 import "amfe-flexible";
-import Vant from "vant";
 import router from "./router/router";
 import pinia from "./stores";
 import "vant/lib/index.css";
@@ -11,5 +12,6 @@ const app = createApp(App);
 app.use(Vant);
 app.use(pinia)
 app.use(router);
+app.use(ElementPlus)
 
 app.mount("#app");
--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,5 +1,46 @@
 <template>
-  <div>login</div>
+  <div class="login_content">
+    <div class="login_img">
+      <van-image
+        width="186"
+        src="https://award.xmcdn.com/yx/fe-passport/last/dist/images/new-logo-m@2x_2909eca.png"
+      />
+    </div>
+    <div class="login_main">
+      <div class="tab">
+        <div class="login_password">
+          <span>密码登录</span>
+        </div>
+        <div class="login_captcha">
+          <span>验证码登录</span>
+        </div>
+      </div>
+      <van-form @submit="onSubmit">
+        <van-cell-group inset>
+          <van-field
+            v-model="username"
+            name="用户名"
+            label="用户名"
+            colon
+            placeholder="请输入用户名"
+          />
+          <van-field
+            v-model="password"
+            type="password"
+            name="密码"
+            label="密码"
+            colon
+            placeholder="请输入密码"
+          />
+        </van-cell-group>
+        <div class="login_button" style="margin: 16px">
+          <van-button round block type="primary" native-type="submit">
+            提交
+          </van-button>
+        </div>
+      </van-form>
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
@@ -8,6 +49,38 @@ export default defineComponent({
   name: "login",
 });
 </script>
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { Image,Form } from "vant";
+</script>
 
-<style scoped></style>
+<style scoped lang="less">
+.login_content {
+  padding: 30px 40px 0px 40px;
+  text-align: center;
+  .login_img {
+    text-align: center;
+    width: 310rox;
+    margin-bottom: 20px;
+  }
+  .login_main {
+    .tab {
+      display: flex;
+      font-size: 13px;
+      .login_password {
+        border-bottom: 2px solid #ff4613;
+        flex: 1;
+        height: 35px;
+        line-height: 35px;
+      }
+      .login_captcha {
+        flex: 1;
+        height: 35px;
+        line-height: 35px;
+      }
+    }
+  }
+  .login_button{
+    margin-top:20px;
+  }
+}
+</style>
