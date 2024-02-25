f2de07e7ada1c545e31fa456c9f05a6436ad928e
jiagaoshang <2308077322@qq.com>
2024-02-25 21:09:37 +0800

    第4.1次

--- src/views/homepage/index.vue
+++ src/views/homepage/index.vue
@@ -1,15 +1,13 @@
 <template>
   <div class="container">
-    <!-- <div class="home_top">
+    <div class="home_top">
       <div class="leftimg">
         <img src="./images//logo.png" alt="" />
       </div>
       <div class="rightbtn">打开APP</div>
-    </div> -->
-    <JgsHead />
+    </div>
     <!-- 搜索框 -->
-    <!-- <van-search shape="round" value="{{ value }}" placeholder="搜索" /> -->
-    <JgsSearch />
+    <van-search shape="round" value="{{ value }}" placeholder="搜索" />
     <!-- 轮播图 -->
     <el-carousel :interval="2000" type="card" height="139px" indicator-position="none">
       <el-carousel-item v-for="item in 6" :key="item">
@@ -159,8 +157,6 @@
   
 <script lang="ts">
 import { defineComponent, ref } from "vue";
-import JgsSearch from "../../components/JgsSearch/index.vue";
-import JgsHead from "../../components/JgsHead/index.vue";
 export default defineComponent({
   name: "homepage",
 });
